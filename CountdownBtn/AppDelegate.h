fd92be52dd58e76c3a3a6d5080e1bb7512733422
tanjianzhong <5652903@qq.com>
2018-05-24 16:01:04 +0800

    Initial Commit

---
+++ CountdownBtn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CountdownBtn
+//
+//  Created by tanjianzhong on 2018/5/24.
+//  Copyright © 2018年 tanjianzhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CountdownBtn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CountdownBtn
+//
+//  Created by tanjianzhong on 2018/5/24.
+//  Copyright © 2018年 tanjianzhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
